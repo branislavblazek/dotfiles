92fe9ddab2bfa550a48bc0c99c782f4da833ab42
Branislav Blazek <blazek@pap.sk>
2024-10-25 21:24:19 +0200

    moved alias files into custom fodler

--- script/alias.sh
+++ script/alias.sh
@@ -6,8 +6,8 @@ pushd . &> /dev/null
 cd "$(dirname "$0")/.."
 DOTFILES_DIR=$(pwd -P)
 
-# Find all files which ext is alias
-for alias_file in "$DOTFILES_DIR"/*.alias; do
+# Find all files in dir alias
+for alias_file in "$DOTFILES_DIR"/alias/*; do
 	if [[ -f "$alias_file" ]]; then
 		source "$alias_file"
 	fi
